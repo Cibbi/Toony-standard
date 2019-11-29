557f37112532a577514476efd3df6fb5ef07b410
Cibbi <12004047+Cibbi@users.noreply.github.com>
2019-11-29 19:30:13 +0100

    Fixed updater

--- Editor/Utility/Updater.cs
+++ Editor/Utility/Updater.cs
@@ -238,7 +238,7 @@ namespace Cibbi.ToonyStandard
                 local.lastCheck = DateTime.Now.AddDays(-2).ToString();
                 File.WriteAllText(TSConstants.LocalJSONPath, JsonUtility.ToJson(local));
             }
-            if(local.lastCheck.Equals(""))
+            if(local.lastCheck != null && local.lastCheck.Equals(""))
             {
                 local.lastCheck = DateTime.Now.AddDays(-2).ToString();
             }
